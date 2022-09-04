2b710d2b68ccd5709e63ee7a986b28e58e241f85
Faheem Qadir <faheem.qadir@salesflo.com>
2022-09-04 19:07:54 +0500

    add rider limit setting

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -231,6 +231,7 @@ class ApiController extends Controller
             INNER JOIN users AS U ON U.id = O.customer_id
             INNER JOIN shops AS S ON S.id = O.shop_id
             WHERE C.id="'.$iCustomerId.'"
+            Order By orders.id DESC
         ');
         //WHERE C.id="'.$iCustId.'"
         foreach($aData as $ind => $val){
